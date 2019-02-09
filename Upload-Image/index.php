2022cc1f11cfc172cb0432e02f9de783bad1ea29
Jii Saaduddin <jiisaaduddin@gmail.com>
2019-02-09 13:33:37 +0800

    not needed files
    test

--- Upload-Image/index.php
+++
@@ -1,9 +0,0 @@
-
-<form method="post" action="upload.php" enctype="multipart/form-data">
-
-	<input type="file" name="photolink">
-
-	<button type="submit" name="Submit">Upload</button>
-
-</form>
-
