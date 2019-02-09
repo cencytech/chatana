a1bd42c1f8a67693883dc248b562e099445b579c
Jii Saaduddin <jiisaaduddin@gmail.com>
2019-02-09 13:31:51 +0800

    capture js
    upload image on group chat

--- admin/room.php
+++ admin/room.php
@@ -43,8 +43,71 @@
 			<input type="text" class="form-control chat_msg" placeholder="Type message..." id="chat_msg">
 			
 			<span class="input-group-btn">
-				<input type="file" class="btn btn-default btn-flat #send_msg">
-				<button class="btn btn-success btn-flat send_msg" type="submit" id="send_msg" value="<?php echo $id; ?>"><span class="fa fa-comment"></span> Send</button>
+
+				<input type="file" class="file1" id="file1" name="image" accept="image/*" capture style="display:none"/>
+				<img src="../upload/icons/camera-icon.png" id="upfile1" style="cursor:pointer" width="32px" />
+
+				<script>
+				  $(document).ready(function(e) {
+						$(".showonhover").click(function(){
+							$("#selectfile").trigger('click');
+						});
+					});
+
+
+				var input = document.querySelector('input[type=file]'); // see Example 4
+
+				input.onchange = function () {
+				  var file = input.files[0];
+
+				  drawOnCanvas(file);   // see Example 6
+				  displayAsImage(file); // see Example 7
+				};
+
+				function drawOnCanvas(file) {
+				  var reader = new FileReader();
+
+				  reader.onload = function (e) {
+					var dataURL = e.target.result,
+						c = document.querySelector('canvas'), // see Example 4
+						ctx = c.getContext('2d'),
+						img = new Image();
+
+					img.onload = function() {
+					  c.width = img.width;
+					  c.height = img.height;
+					  ctx.drawImage(img, 0, 0);
+					};
+
+					img.src = dataURL;
+				  };
+
+				  reader.readAsDataURL(file);
+				}
+
+				function displayAsImage(file) {
+				  var imgURL = URL.createObjectURL(file),
+					  img = document.createElement('img');
+
+				  img.onload = function() {
+					URL.revokeObjectURL(imgURL);
+				  };
+
+				  img.src = imgURL;
+				  document.body.appendChild(img);
+				}
+
+				$("#upfile1").click(function () {
+					$("#file1").trigger('click');
+				});
+				$("#upfile2").click(function () {
+					$("#file2").trigger('click');
+				});
+				$("#upfile3").click(function () {
+					$("#file3").trigger('click');
+				});
+				</script>
+				<button class="btn btn-default btn-fill btn-flat send_msg" type="submit" id="send_msg" value="<?php echo $id; ?>"><span class="fa fa-send fa-lg"></span></button>
 			</span>
 		</div>
 	</div>
