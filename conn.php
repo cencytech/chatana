a93f7ef1e49ff6a149705e00d0c3b36a0aca5a4d
Jii Saaduddin <jiisaaduddin@gmail.com>
2019-02-19 14:29:25 +0800

    connection

--- conn.php
+++ conn.php
@@ -11,12 +11,10 @@ $config = array(
 	
 //MySQLi Procedural
 $conn = mysqli_connect($config["Host"],$config["Root"],$config["Pass"],$config["Db"]);
-
-#$mysqliConnectUpimage = mysqli_connect($config["Host"],$config["Root"],$config["Pass"],$config["Db"]);
-
-
 if (!$conn) {die("Connection failed: " . mysqli_connect_error());}
 
 $connPDO = new PDO( 'mysql:host='.$config["Host"].';dbname=abfiphco_chatana', $config["Root"], $config["Pass"]);
+
+#$mysqliConnectUpimage = mysqli_connect($config["Host"],$config["Root"],$config["Pass"],$config["Db"]);
 
 ?>
